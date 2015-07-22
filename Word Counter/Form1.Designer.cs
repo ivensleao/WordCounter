b0af21cf5f73d747172e08b14e28a6d4a55bfd55
ivensleao <ivensleao@gmail.com>
2015-07-22 12:09:51 -0300

    Iniciando

---
+++ Word Counter/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Word_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAnalysis = new System.Windows.Forms.Button();
+            this.textFile = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClipboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAnalysis
+            // 
+            this.btnAnalysis.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAnalysis.Enabled = false;
+            this.btnAnalysis.Location = new System.Drawing.Point(14, 67);
+            this.btnAnalysis.Name = "btnAnalysis";
+            this.btnAnalysis.Size = new System.Drawing.Size(121, 23);
+            this.btnAnalysis.TabIndex = 0;
+            this.btnAnalysis.Text = "Analisar arquivo";
+            this.btnAnalysis.UseVisualStyleBackColor = false;
+            this.btnAnalysis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textFile
+            // 
+            this.textFile.Location = new System.Drawing.Point(227, 39);
+            this.textFile.Name = "textFile";
+            this.textFile.Size = new System.Drawing.Size(426, 20);
+            this.textFile.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(659, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Informe o arquivo a ser pesquisado:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(14, 127);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(718, 208);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ocorrência das palavras no arquivo";
+            // 
+            // btnClipboard
+            // 
+            this.btnClipboard.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnClipboard.Enabled = false;
+            this.btnClipboard.Location = new System.Drawing.Point(15, 355);
+            this.btnClipboard.Name = "btnClipboard";
+            this.btnClipboard.Size = new System.Drawing.Size(231, 23);
+            this.btnClipboard.TabIndex = 7;
+            this.btnClipboard.Text = "Copiar análise para área de transferência";
+            this.btnClipboard.UseVisualStyleBackColor = false;
+            this.btnClipboard.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(744, 390);
+            this.Controls.Add(this.btnClipboard);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textFile);
+            this.Controls.Add(this.btnAnalysis);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnalysis;
+        private System.Windows.Forms.TextBox textFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClipboard;
+    }
+}
+
---
+++ Word Counter/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Word_Counter
+{
+    public partial class Form1 : Form
+    {
+        private List<string> wordList = new List<string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (wordList != null && wordList.Count > 0)
+            {
+                txtResult.Text = string.Empty;
+
+                Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                foreach (string word in wordList)
+                {
+                    if (word.Length >= 3)
+                    {
+                        if (dictionary.ContainsKey(word))
+                        {
+                            dictionary[word]++;
+                        }
+                        else
+                        {
+                            dictionary[word] = 1;
+                        }
+
+                    }  
+
+                }
+
+                var sortedDict = (from entry in dictionary orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+                MessageBox.Show("Aguarde! O processo demorará de acordo com o tamanho do arquivo a ser analisado!");
+
+                foreach (KeyValuePair<string, int> pair in sortedDict)
+                {
+                    txtResult.Text += pair.Key + "\t  " + pair.Value + " vez(es) \r\n";
+                }
+
+                textFile.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Selecione um arquivo!");
+            }
+
+            wordList = new List<string>();
+
+            btnClipboard.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btnClipboard.Enabled = false;
+            
+            fileDialog.Filter = "Text Files (.txt)|*.txt";
+            fileDialog.FilterIndex = 1;
+
+            DialogResult result = fileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                btnAnalysis.Enabled = true;
+                
+                string fileName = textFile.Text = fileDialog.FileName;
+             
+                string inputString = File.ReadAllText(fileName, Encoding.UTF8);
+
+                // Convert our input to lowercase
+                inputString = inputString.ToLower();
+
+                Regex emailReplace = new Regex(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}", RegexOptions.IgnoreCase);
+                inputString = emailReplace.Replace(inputString, " ");
+
+
+                // Define characters to strip from the input and do it
+                string[] stripChars = { ";", ",", ".", "-", "_", "(", ")", "[", "]", "'",@"""", "::",":",
+                                          "?", "!", "+", "*", @"/", @"\", @"#",@"%",">", "<","=", "{", "}",
+						"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\n", "\t", "\r" };
+                foreach (string character in stripChars)
+                {
+                    inputString = inputString.Replace(character, " ");
+                }
+
+                this.wordList = inputString.Split(' ').ToList();
+
+                //this.wordList = this.wordList.Distinct().ToList();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtResult.Text))
+            {
+                MessageBox.Show("A análise não apresentou resultados");
+            }
+            else
+            {
+                System.Windows.Forms.Clipboard.SetText(txtResult.Text);
+                MessageBox.Show("Texto copiado!");
+            }
+        }
+    }
+}
